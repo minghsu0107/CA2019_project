f2b44ba3571fd64aa2c119abef51c6342a7fdeb9
robert1003 <b07902047@ntu.edu.tw>
2019-12-05 17:47:21 +0800

    HazardDetection.v fix reg

--- code/HazardDetection.v
+++ code/HazardDetection.v
@@ -2,24 +2,31 @@ module HazardDetection (MemRead_IDEX, rd_IDEX, rs1_IFID, rs2_IFID, ID_equal, isB
 
 input [0:0] MemRead_IDEX, ID_equal, isBranch;
 input [4:0] rd_IDEX, rs1_IFID, rs2_IFID;
-output reg [0:0] PCWrite_o, IFIDStall_o, IFIDFlush_o,Mux_o;
+output [0:0] PCWrite_o, IFIDStall_o, IFIDFlush_o, Mux_o;
+
+reg [0:0] tmp_PCWrite_o, tmp_IFIDStall_o, tmp_IFIDFlush_o, tmp_Mux_o;
+
+assign PCWrite_o = tmp_PCWrite_o;
+assign IFIDStall_o = tmp_IFIDStall_o;
+assign IFIDFlush_o = tmp_IFIDFlush_o;
+assign Mux_o = tmp_Mux_o;
 
 always @ ( * ) begin
-    {IFIDStall_o, IFIDFlush_o, Mux_o} <= 0;
-    PCWrite_o = 1'b1;
+    {tmp_IFIDStall_o, tmp_IFIDFlush_o, tmp_Mux_o} <= 0;
+    tmp_PCWrite_o = 1'b1;
 
     if (MemRead_IDEX && rd_IDEX != 0 && (rd_IDEX == rs1_IFID || rd_IDEX == rs2_IFID)) begin // stall
-        PCWrite_o <= 1'b0;
-        IFIDStall_o <= 1'b1;
-        IFIDFlush_o <= 1'b0;
-        Mux_o <= 1'b0;
+        tmp_PCWrite_o <= 1'b0;
+        tmp_IFIDStall_o <= 1'b1;
+        tmp_IFIDFlush_o <= 1'b0;
+        tmp_Mux_o <= 1'b0;
     end
 
     if (ID_equal && isBranch) begin // flush
-        PCWrite_o <= 1'b1;
-        IFIDStall_o <= 1'b0;
-        IFIDFlush_o <= 1'b1;
-        Mux_o <= 1'b1;
+        tmp_PCWrite_o <= 1'b1;
+        tmp_IFIDStall_o <= 1'b0;
+        tmp_IFIDFlush_o <= 1'b1;
+        tmp_Mux_o <= 1'b1;
     end
 end
 
