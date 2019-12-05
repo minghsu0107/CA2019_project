3556067e277c8446af74ea7d84130a5bb80bd4c4
robert1003 <b07902047@ntu.edu.tw>
2019-12-05 14:57:01 +0800

    Udpate IFID initial

--- code/IFID.v
+++ code/IFID.v
@@ -3,6 +3,11 @@ module IFID ( clk , PC_i , PC_o , instr_i , instr_o , IFflush , IFstall );
     input clk,IFflush,IFstall;
     output [31:0] PC_o,instr_o;
     reg [31:0] instr,PC;
+
+    initial begin
+        {instr,PC} <= 0;
+    end
+
     assign instr_o = instr;
     assign PC_o = PC;
     always @ (posedge clk) begin
