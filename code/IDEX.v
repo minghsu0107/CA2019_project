e2c8bcbd9eae05ea4ef2e27f294247e1407dbdf8
robert1003 <b07902047@ntu.edu.tw>
2019-12-05 11:02:20 +0800

    commit always block

--- code/IDEX.v
+++ code/IDEX.v
@@ -11,13 +11,14 @@ module IDEX(clk_i,rs1_data,rs2_data,Iimm,Simm,rs1_addr,rs2_addr,rd_addr,funct3,f
     output [1:0] Mem_o;
     output WB_o;
 
+    reg [3:0] tmp;
+    reg [9:0] funct;
+    /*
     reg [31:0] tmp_val1,tmp_val2,tmp_Simm_o;
     reg [3:0] tmp_ALUCtrl;
     reg [4:0] tmp_rs1_addr_o,tmp_rs2_addr_o,tmp_rd_addr_o;
     reg [1:0] tmp_Mem_o;
     reg tmp_WB_o;
-    reg [3:0] tmp;
-    reg [9:0] funct;
 
     assign val1 = tmp_val1;
     assign val2 = tmp_val2;
@@ -40,6 +41,16 @@ module IDEX(clk_i,rs1_data,rs2_data,Iimm,Simm,rs1_addr,rs2_addr,rd_addr,funct3,f
         tmp_WB_o = WB;
         tmp_ALUCtrl = tmp;
     end
+    */
+    assign val1 = rs1_data;
+    assign val2 = ( ALUSrc ? Iimm : rs2_data );
+    assign Simm_o = Simm;
+    assign rs1_addr_o = rs1_addr;
+    assign rs2_addr_o = rs2_addr;
+    assign rd_addr_o = rd_addr;
+    assign Mem_o = Mem;
+    assign WB_o = WB;
+    assign ALUCtrl = tmp;
     // control
     always @(*) begin
         funct = {funct7,funct3};
--- code/MEMWB.v
+++ code/MEMWB.v
@@ -6,6 +6,12 @@ module MEMWB(clk_i,WB_i,MemRdata_i,ALUres_i,rd_addr_i,WBSrc_i,WB_o,MemRdata_o,AL
     output [4:0] rd_addr_o;
     output WB_o,WBSrc_o;
 
+    assign WB_o = WB_i;
+    assign MemRdata_o = MemRdata_i;
+    assign ALUres_o = ALUres_i;
+    assign rd_addr_o = rd_addr_i;
+    assign WBSrc_o = WBSrc_i;
+    /*
     reg [31:0] tmp_MemRdata_o,tmp_ALUres_o;
     reg [4:0] tmp_rd_addr_o;
     reg tmp_WB_o,tmp_WBSrc_o;
@@ -23,6 +29,7 @@ module MEMWB(clk_i,WB_i,MemRdata_i,ALUres_i,rd_addr_i,WBSrc_i,WB_o,MemRdata_o,AL
         //rd_addr_o = rd_addr_i;
         tmp_WBSrc_o = WBSrc_i;
     end
+    */
 
 
 endmodule
